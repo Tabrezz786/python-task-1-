c995278c53d21745bbc7a7bee1fdbf4edc85bcb5
Tabrezz786 <106949069+Tabrezz786@users.noreply.github.com>
2023-01-30 12:51:08 +0530

    Add files via upload

---
+++ Untitled1.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+task = ' this is my first python programming class and i am learning python string and its function '
+1 try to extract data from index 1 to 300 with a jump of 3
+2 try to reverse a string without using function
+3 try split a string after conversion after conversion of entire string into upper case 
+4 try to conver the whole string to lower case
+5 try to capitalize the whole string 
+6 write  a diffrence between isalphanum and isalnum()
+7 try to give an example of expandtabs
+8 give an example of strip , lstrip and rstrip 
+9 replace a string character by another character by taking your own example 
+10 try to give definition of string centre function withoit example 
+11 write your own definition of compiler and interpretor without copying from internet 
+12python is a compiled language give a clear answer with your understanding 
+13 try to write usecase of python with your understanding 
+
+
+# In[3]:
+
+
+t = "this is my first python programming class and i am learning python string and its function"
+
+
+# In[5]:
+
+
+t[1:300:3]
+
+
+# In[6]:
+
+
+t[::-1]
+
+
+# In[7]:
+
+
+t.upper()
+
+
+# In[8]:
+
+
+t = t.upper()
+
+
+# In[9]:
+
+
+t.split(" ")
+
+
+# In[10]:
+
+
+t
+
+
+# In[11]:
+
+
+t.lower()
+
+
+# In[12]:
+
+
+t.capitalize()
+
+
+# t.isalpha  .A string is alphabetic if all characters in the string are alphabetic and there
+# is at least one character in the string.
+# 
+# t.isalnum  .A string is alpha-numeric if all characters in the string are alpha-numeric and
+# there is at least one character in the string.
+# 
+
+# In[16]:
+
+
+g = "asdfbg"
+
+
+# In[17]:
+
+
+g.isalpha()
+
+
+# In[18]:
+
+
+l = "54665436"
+
+
+# In[19]:
+
+
+l.isalnum()
+
+
+# In[21]:
+
+
+y = " ineuron\tiineuron\tiiineuron"
+
+
+# In[22]:
+
+
+y.expandtabs()
+
+
+# In[23]:
+
+
+f= '     tabrez   '
+
+
+# In[24]:
+
+
+f.strip()
+
+
+# In[25]:
+
+
+f.lstrip()
+
+
+# In[26]:
+
+
+f.rstrip()
+
+
+# replacing a string character 
+# 
+
+# In[32]:
+
+
+T = "sudhanshu"
+
+
+# In[33]:
+
+
+T + "786"
+
+
+# t.center = Return a centered string of length width.
+# 
+# Padding is done using the specified fill character (default is a space).
+# 
+
+# In[44]:
+
+
+t.center(150, "&")
+
+
+#  p = """python is compiled language beacuse it is case sensitive and data sets are to programmed with similar kind and cannot be 
+# performed with other data set"""
+# 
+
+# python is used for developing logic to create business logic and pipeline is its main purpose , it can also be used for  game development , backend cosing and it has ,any other uses cases etc.
+# 
+
+# compiler is a software which gathers the data and gives us an output which is done by interpretor 
+# there are types of compiler for example jupyter notebook 
+
+# In[ ]:
+
+
+sir i have  doubt in understanding replacement of string character by another character can you help me on it 
+
